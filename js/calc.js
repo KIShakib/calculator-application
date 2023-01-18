abd6daa6bb77b778ca78912da66b5e0fe04c7919
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-01-19 00:48:37 +0600

    Hover Effect Done

---
+++ js/calc.js
@@ -0,0 +1,31 @@
+const del = () => {
+    const value = document.getElementById('screen').value;
+    document.getElementById('screen').value = value.substr(0, value.length - 1);
+}
+
+const one = document.getElementById('one');
+one.addEventListener('click', function () {
+    const body = document.querySelector('body');
+    const toggle = document.getElementById('circle');
+    body.classList.remove('active1');
+    body.classList.remove('active2');
+    toggle.style.left = '0';
+});
+
+const two = document.getElementById('two');
+two.addEventListener('click', function () {
+    const body = document.querySelector('body');
+    const toggle = document.getElementById('circle');
+    body.classList.add('active1');
+    body.classList.remove('active2');
+    toggle.style.left = '36%';
+})
+
+const three = document.getElementById('three');
+three.addEventListener('click', function () {
+    const body = document.querySelector('body');
+    const toggle = document.getElementById('circle');
+    body.classList.add('active2');
+    body.classList.remove('active1');
+    toggle.style.left = '65%';
+})
